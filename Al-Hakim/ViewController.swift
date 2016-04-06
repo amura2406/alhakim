db7995870e19876568797940d6952a45eaf44294
Angga Rahadian <amura_2406@yahoo.com>
2016-04-07 05:24:52 +0700

    Initial Commit

---
+++ Al-Hakim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Al-Hakim
+//
+//  Created by Angga Muhammad Rahadian on 4/7/16.
+//  Copyright © 2016 Al-Hakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
